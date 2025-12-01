6da3b1875307fee71d7efb564997876f799e6886
Daniel Ferrari <daniel.ferrari@filewave.com>
2025-12-01 14:39:28 +0100

    refactor: adding better comments

--- src/types/transactions.ts
+++ src/types/transactions.ts
@@ -155,7 +155,7 @@ export interface Transaction {
   subAmount: unknown;
   status: 'EXECUTED' | 'CANCELED';
   action: TransactionAction;
-  eventType?: TRANSACTION_EVENT_TYPE; // Made optional since it's no longer in the API response
+  eventType?: TRANSACTION_EVENT_TYPE; // Added later to the transaction
   cashAccountNumber: string | null;
   hidden: boolean;
   deleted: boolean;
